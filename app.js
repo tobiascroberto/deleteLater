dd0927ecc207465996b6570b5d0e6a6cea2e9ccd
Roberto Tobias <tobiasc.roberto@gmail.com>
2024-12-18 02:09:57 -0600

    Simplificar y configurar para VERVEL

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const axios = require('axios')
+
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
+
+
+
+const app = express()
+
+app.use(express.text())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.status(201).send('<h1> Hola Mundo desde Express</h1>')
+})
+
+
+app.post('/productos', (req,res)=>{
+
+    const obj = req.body
+
+    res.status(201).json(obj)
+})
+
+app.post('/productos2', (req,res)=>{
+
+    const {name, age} = req.body
+
+    res.status(201).send(`<h2>Saludos: ${name} edad: ${age}</h2>`)
+})
+
+app.get('/post', async(req, res)=>{
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+    console.log(respuesta.data.length)
+
+    res.json(respuesta.data)
+})
+
+app.post('/imagen', upload.single('imagen'), (req,res)=>{
+
+    const imagen = req.file
+
+    console.log(imagen)
+    res.status(201).send('Saludos desde imagen')
+})
+
+
+app.get('/:name',(req,res)=>{
+    const name = req.params.name
+    res.status(202).send(`<h1>Saludos ${name}`)
+})
+
+
+const PORT = process.env.PORT || 4000
+console.log(PORT)
+app.listen(PORT)
